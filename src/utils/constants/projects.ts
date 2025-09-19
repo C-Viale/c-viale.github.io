f38971576291dda5d53c6f45a8e1208eb22f6a3c
Caio Lima <caiovialedl@gmail.com>
2025-09-19 00:42:42 -0300

    improve homepage

--- src/utils/constants/projects.ts
+++ src/utils/constants/projects.ts
@@ -7,9 +7,33 @@ type Project = {
 
 export const projects: Project[] = [
   {
+    id: "learn_opengl_br",
+    name: "AprendaOpenGL",
+    description: "",
+    href: "/#",
+  },
+  {
     id: "athens_portal",
     name: "Athens Portal",
     description: "",
     href: "/projects/athens-portal",
+  },
+  {
+    id: "conquest",
+    name: "Conquest",
+    description: "",
+    href: "/#",
+  },
+  {
+    id: "rust_lexer",
+    name: "Lexer with Rust",
+    description: "",
+    href: "/#",
+  },
+  {
+    id: "3d_rendering",
+    name: "3D Renderer with OpenGL",
+    description: "",
+    href: "/#",
   },
 ];
