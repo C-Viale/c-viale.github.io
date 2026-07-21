eac3d91a84069ac7b5a4ed6cc46141fc4b494e2e
Caio Lima <caiovialedl@gmail.com>
2026-07-21 17:51:33 -0300

    improve work experience details

--- src/utils/constants/work-experiences.ts
+++ src/utils/constants/work-experiences.ts
@@ -6,6 +6,7 @@ type WorkExperience = {
   period: string[];
   details: string[];
   freelancer?: boolean;
+  mainSkills: string[];
 };
 
 type Milestone = {
@@ -13,7 +14,7 @@ type Milestone = {
   afterId?: number;
   label: string;
   period: string;
-}
+};
 
 export const milestones: Milestone[] = [
   {
@@ -26,7 +27,7 @@ export const milestones: Milestone[] = [
     label: "Started Freelancing",
     period: "November 2023",
   },
-]
+];
 
 export const workExperiences: WorkExperience[] = [
   {
@@ -34,7 +35,8 @@ export const workExperiences: WorkExperience[] = [
     company: "Like Brands",
     position: "Full-Stack Engineer",
     period: ["June 2024", "present"],
-    location: "Remote",
+    location: "Remote, Brazil",
+    mainSkills: ["TypeScript", "React", "C#", "ASP.NET Core", "PostgreSQL"],
     details: [
       `Developed and implemented new features for the “GlorySoft” project, a digital platform for a goods distribution company.`,
       `Refactored most of old architecture, improving development and maintanence time.`,
@@ -45,8 +47,9 @@ export const workExperiences: WorkExperience[] = [
     company: "JatLavo",
     position: "Full-Stack Engineer",
     period: ["June 2024", "December 2025"],
-    location: "Remote",
+    location: "Remote, Brazil",
     freelancer: true,
+    mainSkills: ["C#", "ASP.NET Core", "SQLite"],
     details: [`Developed and implemented an internal management system.`],
   },
   {
@@ -54,8 +57,9 @@ export const workExperiences: WorkExperience[] = [
     company: "Spark",
     position: "Full-Stack Engineer",
     period: ["March 2025", "May 2025"],
-    location: "Remote",
+    location: "Remote, Brazil",
     freelancer: true,
+    mainSkills: ["TypeScript", "React", "PostgreSQL"],
     details: [`Developed and implemented new features for the company's ORM.`],
   },
   {
@@ -63,7 +67,8 @@ export const workExperiences: WorkExperience[] = [
     company: "Shaw and Partners",
     position: "Full-Stack Engineer",
     period: ["November 2023", "July 2024"],
-    location: "Remote",
+    location: "Remote, USA",
+    mainSkills: ["TypeScript", "React", "Node.js", "PostgreSQL", "Docker"],
     details: [
       `Engaged with a multicultural team across diﬀerent time zones, using English as the primary language for meetings, documentation, and cross-functional communication.`,
       `Developed and implemented new features for the company's business management system, including HR pipelines, tasks management, billing and automated reports.`,
@@ -74,7 +79,15 @@ export const workExperiences: WorkExperience[] = [
     company: "Okalab",
     position: "Lead Full-Stack Engineer",
     period: ["August 2022", "November 2023"],
-    location: "Bebedouro, SP",
+    location: "Bebedouro, SP, Brazil",
+    mainSkills: [
+      "TypeScript",
+      "React",
+      "Node.js",
+      "PostgreSQL",
+      "Docker",
+      "AWS",
+    ],
     details: [
       `Led a team with 4 developers.`,
       `Designed the entire architecture of the “PrevLife” project, a digital platform presented to the Love Hospital, the largest oncology hospital in Brazil.`,
@@ -88,7 +101,15 @@ export const workExperiences: WorkExperience[] = [
     company: "Cifal Distribuidora",
     position: "Full-Stack Engineer",
     period: ["July 2020", "April 2022"],
-    location: "Bebedouro, SP",
+    location: "Bebedouro, SP, Brazil",
+    mainSkills: [
+      "JavaScript",
+      "TypeScript",
+      "React",
+      "Node.js",
+      "MSSQL",
+      "PostgreSQL",
+    ],
     details: [
       `Developed and implemented new features on the old internal web portal.`,
       `Developed a new internal web portal from scratch.`,
